5b79db624dae856ecb1c90c19a3a2f00618b8921
Yasmin Farisah Salma <114626408+caernations@users.noreply.github.com>
2024-10-05 20:35:17 +0700

    fix: theme

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -9,14 +8,14 @@ const Color pinkclr = Color(0xFFFf4667);
 const Color white = Colors.white;
 const primaryClr = bluishClr;
 const Color darkGreyClr = Color (0xFF121212);
-Color darkHeaderClr = Color (0xFF424242);
+Color darkHeaderClr = const Color (0xFF424242);
 
 class Themes {
   static final light = ThemeData(
     primaryColor: bluishClr,
     brightness: Brightness.light,
-    appBarTheme: AppBarTheme(
-      backgroundColor: bluishClr,  // AppBar color for light theme
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,  // AppBar color for light theme
       iconTheme: IconThemeData(color: Colors.white),
       titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
     ),
@@ -27,8 +26,8 @@ class Themes {
     brightness: Brightness.dark,
     appBarTheme: AppBarTheme(
       backgroundColor: darkHeaderClr,  // AppBar color for dark theme
-      iconTheme: IconThemeData(color: Colors.white),
-      titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      iconTheme: const IconThemeData(color: Colors.white),
+      titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
     ),
   );
 }
